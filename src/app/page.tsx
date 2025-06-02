8c5c17456ac25d41a7aac9c391347222561c293b
Arthur Ho <arthur_hohoho@hotmail.com>
2025-06-02 18:01:35 +1000

    Add OpenTelemetry trace to Seq

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,25 @@
 import { auth } from '@/auth';
+import { tracer } from '@/lib/trace';
 
 export default async function Home() {
     const session = await auth();
 
-    if (!session) return (
-        <div>Not authenticated</div>
-    );
+    if (!session) {
+        return await tracer.startActiveSpan('Not authenticated', async (span) => {
+            try {
+                return (
+                    <div>Not authenticated</div>
+                );
+
+            } catch (err: unknown) {
+                const exception = err as Error;
+                span.recordException(exception);
+                throw err
+            } finally {
+                span.end();
+            }
+        });
+    }
 
     return (
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
